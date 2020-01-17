7c6d5df8b3c973475985993ee643c1df5302d23e
Smith <HJ28A18S@usfc.com>
2020-01-17 14:31:53 -0500

    added package loading

--- packageTest.R
+++ packageTest.R
@@ -1,14 +1,18 @@
-pkgTest <- function(x){
-  # This function checks to see if required packages (x) are installed and installs them if needed
+pkgTest <- function(packages){
+  # This function checks to see if required packages are installed 
+  #   and installs them if needed, and loads them
   # 
   # args: character vector of packages to be installed
   # 
   # returns: sentence finish
-  new.packages <- x[!(x %in% installed.packages()[,'Package'])]
+  new.packages <- packages[!(packages %in% installed.packages()[,'Package'])]
   if(length(new.packages)){
     install.packages(new.packages)
     print('Packages installed.', quote = F)
   } else {
-    print('No new packages needed.', quote = F)
+    cat('No new packages needed. \nPackages loaded.')
+  }
+  for(package in packages){
+    suppressPackageStartupMessages(library(package, character.only = T, quietly = T))
   }
 }
